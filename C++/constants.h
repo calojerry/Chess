17fad1b9643518cededbb149e8ca5bce46057b72
calojerry <c.gonzales0528@gmail.com>
2025-01-01 22:52:47 -0600

    Update constants.h

--- C++/constants.h
+++ C++/constants.h
@@ -4,7 +4,7 @@
 #include <string>
 #include <unordered_map>
 
-// Unicode symbols for pieces
+// unicode symbols for pieces
 const std::unordered_map<char, std::string> UNICODE_PIECE_SYMBOLS = {
     {"R", "♖"}, {"r", "♜"},
     {"N", "♘"}, {"n", "♞"},
@@ -14,13 +14,13 @@ const std::unordered_map<char, std::string> UNICODE_PIECE_SYMBOLS = {
     {"P", "♙"}, {"p", "♟"},
 };
 
-// File names (columns a-h)
+// file names 
 const std::string FILE_NAMES[8] = {"a", "b", "c", "d", "e", "f", "g", "h"};
 
-// Rank names (rows 1-8)
+// rank names
 const std::string RANK_NAMES[8] = {"1", "2", "3", "4", "5", "6", "7", "8"};
 
-// FEN strings for starting position
+// fen strings for starting position
 const std::string STARTING_FEN = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
 const std::string STARTING_BOARD_FEN = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR";
 
