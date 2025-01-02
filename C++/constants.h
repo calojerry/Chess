c26d5ecbcbdec023b2dacde3e170d24f4f4a9fd6
calojerry <c.gonzales0528@gmail.com>
2025-01-02 15:55:19 -0600

    Update constants.h

--- C++/constants.h
+++ C++/constants.h
@@ -3,6 +3,14 @@
 
 #include <string>
 #include <unordered_map>
+#include <cstdint> // for 64-bit unsigned integer
+
+using U64 = std::uint64_t;
+
+const U64 notA = 18374403900871474942ULL; // not A file constant
+const U64 notH = 9187201950435737471ULL; // not H file constant
+const U64 notHG = 4557430888798830399ULL; // not HG file constant
+const U64 notAB = 18229723555195321596ULL; // not AB file constant
 
 // unicode symbols for pieces
 const std::unordered_map<char, std::string> UNICODE_PIECE_SYMBOLS = {
