cf524d0f1df7b82f928b6ca636719acb62d821c2
calojerry <c.gonzales0528@gmail.com>
2025-01-01 22:53:46 -0600

    Update chess.h

--- C++/chess.h
+++ C++/chess.h
@@ -8,10 +8,6 @@
 
 class Chess {
 private:
-  Bitboard board;
-  Color currTurn = Color::WHITE; // default: white to move
-  bool gameOver = false; // default: game is not over
-  bool isDraw;
 
 public:
   Chess();
